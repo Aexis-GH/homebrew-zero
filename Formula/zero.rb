7d763a64258d95fcdd9f8a31d809949149df8f9e
B <3469442+aexis-b@users.noreply.github.com>
2026-01-06 05:35:34 -0600

    Update zero formula to 0.1.12

--- Formula/zero.rb
+++ Formula/zero.rb
@@ -1,8 +1,8 @@
 class Zero < Formula
   desc "Aexis Zero scaffolding CLI"
   homepage "https://www.npmjs.com/package/@aex.is/zero"
-  url "https://registry.npmjs.org/@aex.is/zero/-/zero-0.1.11.tgz"
-  sha256 "fe7123d5a0c433bdf50c4064aa16da481879f11235c83c3a5aebe3e821087600"
+  url "https://registry.npmjs.org/@aex.is/zero/-/zero-0.1.12.tgz"
+  sha256 "e6c1990d6e4a17d23e22e7d9624b1ae6e345e690076c608d7b5644f4a968bd84"
   license "UNLICENSED"
 
   depends_on "node"
